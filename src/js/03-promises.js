ea4563da34519ecc8ccafcb6e4300427249d6a51
MrRoik <138239220+MrRoik@users.noreply.github.com>
2023-11-27 22:39:33 +0200

    create task 3 - js
    task - 3 js is done
    add style in common.css

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,56 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const selectors = {
+  form: document.querySelector('form'),
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]')
+}
+
+selectors.form.addEventListener("click", createPromisesClick)
+
+const options = {
+  position: 'center-bottom',
+  distance: '30px',
+  borderRadius: '5px',
+  opacity: 0.8,
+  timeout: 8000,
+  clickToClose: true,
+  cssAnimationStyle: 'zoom',  //from-right
+};
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function createPromisesClick(evt) {
+  evt.preventDefault();
+
+  let formDelay = +selectors.delay.value;
+  let formStep = +selectors.step.value;
+  let formAmount = +selectors.amount.value;
+
+  for (let i = 1; i <= formAmount; i++) {
+    formDelay += formStep;
+    
+    createPromise(i, formDelay)
+      .then(({ position, delay }) => {
+         // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, options);
+     })
+      .catch(({ position, delay }) => {
+        // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, options);
+       });
+    evt.currentTarget.reset();
   }
 }
